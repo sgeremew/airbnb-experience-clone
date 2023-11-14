f5a1672c782c98d09f0cc15923a2166f7e5ca27b
sgeremew <geremews@yahoo.com>
2023-11-14 00:37:23 -0500

    fixed css styling by adding style import and added hero component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import airbnbLogo from "../images/airbnb-logo.png"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/airbnb-logo.png" alt="airbnb logo"/>
+            <img className="nav--logo" src={airbnbLogo} alt="airbnb logo"/>
         </nav>
     );
 }
